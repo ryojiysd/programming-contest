78ee5c86dd6a488f8db5bc4a91dd76dc6144b99e
Ryoji Yoshida <ryoji.ysd@gmail.com>
2020-05-31 10:24:47 +0900

    Add template

---
+++ library/README.md
@@ -0,0 +1,7 @@
+# 競プロライブラリ集
+
+## 汎用
+| Name | Code | Description |
+| :-- | :-- | :-- |
+| テンプレート | [template.cpp](template.cpp) | 毎回使用するマクロ等 |
+
---
+++ library/template.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define sz(x) int(x.size())
+#define ALL(c) (c).begin(), (c).end()
+#define SUM(x) std::accumulate(ALL(x), 0LL)
+#define MIN(v) *std::min_element(v.begin(), v.end())
+#define MAX(v) *std::max_element(v.begin(), v.end())
+#define EXIST(v, x) (std::find(v.begin(), v.end(), x) != v.end())
+#define DUMP(v) for(auto& x : v) cerr << x << " "; cerr << endl;
+
+using namespace std;
+using ll = int64_t;
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+constexpr int INF = 1001001001;
+constexpr long long INFL = (1LL<<60);
+constexpr double eps = (1e-9);
+
+int main()
+{
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  
+
+  return 0;
+}
