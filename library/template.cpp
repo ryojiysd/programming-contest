de703531f23c55a2fcb0c4eacde9007d31d58beb
Ryoji Yoshida <ryoji.ysd@gmail.com>
2020-06-07 12:18:47 +0900

    Add dump for vector<pair> and vector<vector>.

--- library/template.cpp
+++ library/template.cpp
@@ -6,13 +6,16 @@
 #define MIN(v) *std::min_element(v.begin(), v.end())
 #define MAX(v) *std::max_element(v.begin(), v.end())
 #define EXIST(v, x) (std::find(v.begin(), v.end(), x) != v.end())
-#define DUMP(v) for(auto& x : v) cerr << x << " "; cerr << endl;
 
 using namespace std;
 using ll = int64_t;
 
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+template<class T> inline void dump(vector<T> v) { for (auto& x : v) cerr << x << " "; cerr << endl; }
+template<class T> inline void dump(vector<pair<T, T>> v) { for (auto& p : v) cerr << p.first << " " << p.second << endl; }
+template<class T> inline void dump(vector<vector<T>> vv) { for (auto& v : vv) {for (auto& x : v) cerr << x << " "; cerr << endl;} }
+
 constexpr int INF = 1001001001;
 constexpr long long INFL = (1LL<<60);
 constexpr double eps = (1e-9);
